acd3a720352f97865c4d3ef9d5435cb01b4d0326
Nathan H. Bean <nhbean@ksu.edu>
2019-11-18 13:34:11 -0600

    Initial commit

---
+++ Movies/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movies
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+
+        public string MPAA_Rating { get; set; }
+
+        public string Major_Genre { get; set; }
+
+        public float? IMDB_Rating { get; set; }
+
+        public float? Rotten_Tomatoes_Rating { get; set; }
+    }
+}
---
+++ Movies/MovieDatabase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Movies
+{
+    /// <summary>
+    /// A class representing a database of movies
+    /// </summary>
+    public class MovieDatabase
+    {
+        private List<Movie> movies = new List<Movie>();
+
+        /// <summary>
+        /// Loads the movie database from the JSON file
+        /// </summary>
+        public MovieDatabase() {
+            
+            using (StreamReader file = System.IO.File.OpenText("movies.json"))
+            {
+                string json = file.ReadToEnd();
+                movies = JsonConvert.DeserializeObject<List<Movie>>(json);
+            }
+        }
+
+        public List<Movie> All { get { return movies; } }
+    }
+}
