be8348f5922acf8c371eba3705469990fe527a97
Timothy Tucker <timoth54@users.campus>
2019-11-18 15:12:41 -0600

    Update

--- Movies/MovieDatabase.cs
+++ Movies/MovieDatabase.cs
@@ -27,5 +27,43 @@ namespace Movies
         }
 
         public List<Movie> All { get { return movies; } }
+
+        public List<Movie> SearchAndFilter(string searchString, List<string> rating)
+        {
+            if (searchString == null && rating.Count == 0) return All;
+            List<Movie> results = new List<Movie>();
+            foreach(Movie movie in movies)
+            {
+                //Case 1: Search string AND ratings
+                if (searchString != null && rating.Count > 0)
+                {
+                    if (movie.Title != null 
+                        && movie.Title.Contains(searchString, StringComparison.InvariantCultureIgnoreCase)
+                        && rating.Contains(movie.MPAA_Rating))
+                    {
+                        results.Add(movie);
+                    }
+                }
+
+                //Case 2: Search string only
+                else if (searchString != null)
+                {
+                    if (movie.Title != null && movie.Title.Contains(searchString, StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        results.Add(movie);
+                    }
+                }
+                //Case 3: ratings only
+                else if (rating.Count > 0)
+                {
+                    if (rating.Contains(movie.MPAA_Rating))
+                    {
+                        results.Add(movie);
+                    }
+                }
+            }
+
+            return results;
+        }
     }
 }
--- Movies/Pages/Index.cshtml.cs
+++ Movies/Pages/Index.cshtml.cs
@@ -9,10 +9,17 @@ namespace Movies.Pages
 {
     public class IndexModel : PageModel
     {
+        public MovieDatabase MovieDatabase = new MovieDatabase();
+        public List<Movie> Movies;
 
         public void OnGet()
         {
+            Movies = MovieDatabase.All;
+        }
 
+        public void OnPost(string search, List<string> rating)
+        {
+            Movies = MovieDatabase.SearchAndFilter(search, rating);
         }
     }
 }
